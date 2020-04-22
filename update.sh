05ad741734fcfef9646d0cff1c7c85d80cb2b47d
Hieu Tran Trung <hieu2t.system@gmail.com>
2020-04-22 14:00:03 +0700

    first commit

--- update.sh
+++ update.sh
@@ -5,4 +5,4 @@ wget http://f.9hits.com/9hviewer/9h-patch-linux-x64.zip
 mv 9h-patch-linux-x64.zip /usr/local/tmpfs/9Hits/9HitsViewer_x64/
 cd /usr/local/tmpfs/9Hits/9HitsViewer_x64/
 unzip -o 9h-patch-linux-x64.zip
-./update.sh
+cd /usr/local/tmpfs/9Hits/
