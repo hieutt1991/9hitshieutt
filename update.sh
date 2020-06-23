05343797a97fe13a5b585ea41fec2677700e60bf
hieutt1991 <hieu2t.system@gmail.com>
2020-06-24 05:08:52 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 cd /usr/local/tmpfs/9Hits/
 pkill 9h
 wget http://f.9hits.com/9hviewer/9h-patch-linux-x64.zip
-cd /usr/local/tmpfs/9Hit
+cd /usr/local/tmpfs/9Hits
 mv 9h-patch-linux-x64.zip /usr/local/tmpfs/9Hits/9HitsViewer_x64/
 cd /usr/local/tmpfs/9Hits/9HitsViewer_x64/
 unzip -o 9h-patch-linux-x64.zip
-cd /usr/local/tmpfs/9Hit
+cd /usr/local/tmpfs/9Hits
 rm 9h-patch-linux-x64.zip
