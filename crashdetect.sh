762cdfd81f4423dd29d4756f3094eb854d0d210f
hieutt1991 <hieu2t.system@gmail.com>
2020-10-21 15:54:13 +0700

    Update crashdetect.sh

--- crashdetect.sh
+++ crashdetect.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-while [[ ! $(pidof 9hmultiss) ]]; do
+while [[ ! $(pidof 9hits) ]]; do
         killall 9htl 9hviewer 9hbrowser 9hmultiss
         Xvfb :1 &
-        export DISPLAY=:1 && /usr/local/tmpfs/9Hits/9HitsViewer_x64/9h_start.sh > /dev/null
+        export DISPLAY=:1 && /usr/local/tmpfs/9Hits/9HitsViewer_x64/9hits > /dev/null
         exit
 done
