d42afa4a2c435a2675a07a429cc65ad6d749f27e
hieutt1991 <hieu2t.system@gmail.com>
2021-02-05 23:32:24 +0700

    Update 9hits.sh

--- 9hits.sh
+++ 9hits.sh
@@ -3,4 +3,4 @@ mkdir -p /usr/local/tmpfs/
 chmod 777 /usr/local/tmpfs/
 mount -t tmpfs -o size=6G tmpfs /usr/local/tmpfs
 cd /usr/local/tmpfs
-git clone https://github.com/hieutt1991/9hitshieutt.git && chmod -R 777 9hitshieutt && 9hitshieutt/install.sh "2" "3e685f2e58dff6f937105299675bc695" "30" "10" "2" "2" "vps248.121"  && rm /usr/local/tmpfs/9Hits/install.sh
+git clone https://github.com/hieutt1991/9hitshieutt.git && chmod -R 777 9hitshieutt && 9hitshieutt/install.sh "2" "3e685f2e58dff6f937105299675bc695" "30" "10" "2" "2" "TCDL"  && rm /usr/local/tmpfs/9Hits/install.sh
