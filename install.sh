e7fe2bcb4c5788cbbfd8217f01cbe45d5146e212
hieutt1991 <hieu2t.system@gmail.com>
2021-02-05 23:19:14 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -456,7 +456,7 @@ EOFSS
 cat > $cronfile <<EOFSS
 * * * * * /usr/local/tmpfs/9Hits/crashdetect.sh
 $cronvar
-58 7 * * * /usr/local/tmpfs/9Hits/reboot.sh
+58 6 * * * /usr/local/tmpfs/9Hits/reboot.sh
 $lookup
 EOFSS
     cd /usr/local/tmpfs
