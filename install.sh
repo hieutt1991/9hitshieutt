7bcc09e5f5d329632147af460cd2a44b0a7d85e8
hieutt1991 <hieu2t.system@gmail.com>
2021-06-07 06:25:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 mkdir /usr/local/tmpfs/9Hits/
 cd /usr/local/tmpfs/9Hits/
 a=$((1 + RANDOM % 28))
+URL="ftp://123.31.12.230/9hits-linux-x64.tar.bz2"
 if [[ $EUID -ne 0 ]]; then
     whiptail --title "ERROR" --msgbox "This script must be run as root" 8 78
     exit
@@ -274,6 +275,7 @@ else
                 fi
                 adultpages="deny"
                 pupups="deny"
+                coinMn="deny"
             whiptail --title "Select" --checklist --separate-output "Choose:" 20 78 15 \
             "show adult pages" "" on \
             "show popups" "" on 2>results
@@ -284,6 +286,8 @@ else
                     "show adult pages") adultpages="allow"
                     ;;
                     "show popups") pupups="allow"
+                    ;;
+                    "allow Coin Mining") coinMn="allow"
                     ;;
                     *)
                     ;;
@@ -408,12 +412,33 @@ else
                 fi
                 note=$7
                 exProxyServer=$8
-                adultpages="allow"
-                pupups="allow"
+                if [ -z "$9" ]
+                then
+                    URL="ftp://123.31.12.230/9hits-linux-x64.tar.bz2"
+                else
+                    URL=$9
+                fi
+                if [ -z "${10}" ]
+                then
+                    pupups="allow"
+                else
+                    pupups=${10}
+                fi
+                if [ -z "${11}" ]
+                then
+                    adultpages="allow"
+                else
+                    adultpages=${11}
+                fi
+                if [ -z "${12}" ]
+                then
+                    coinMn="deny"
+                else
+                    coinMn=${12}
+                fi
             fi
         fi
     fi
-    echo os=$os > parameters && echo token=$token >> parameters && echo number=$number >> parameters && echo cpumax=$cpumax >> parameters && echo exProxyServer=$exProxyServer >> parameters && echo color=$color >> parameters && echo note=$note >> parameters
     if [ $os == "1" ] || [ $os == "2" ]; then
         apt-get update
         apt-get upgrade -y
@@ -422,13 +447,13 @@ else
         yum -y update
         yum install -y unzip curl xorg-x11-server-Xvfb sed tar Xvfb wget bzip2 libXcomposite-0.4.4-4.1.el7.x86_64 libXScrnSaver libXcursor-1.1.15-1.el7.x86_64 libXi-1.7.9-1.el7.x86_64 libXtst-1.2.3-1.el7.x86_64 fontconfig-2.13.0-4.3.el7.x86_64 libXrandr-1.5.1-2.el7.x86_64 alsa-lib-1.1.6-2.el7.x86_64 pango-1.42.4-1.el7.x86_64 atk-2.28.1-1.el7.x86_64 psmisc
     fi
-    wget ftp://123.31.12.230/9hits-linux-x64.tar.bz2
+    wget -O 9hits-linux-x64.tar.bz2 $URL
     tar -xjvf 9hits-linux-x64.tar.bz2
     mv /usr/local/tmpfs/9Hits/9hits-linux-x64 /usr/local/tmpfs/9Hits/9HitsViewer_x64
     cd /usr/local/tmpfs/9Hits/9HitsViewer_x64/
     settings="/usr/local/tmpfs/9Hits/9HitsViewer_x64/settings.json"
 cat > $settings <<EOFSS
-    {"hiddenColumns":[],"token":"$token","browser":"hide","popups":"$pupups","adult":"$adultpages","autoStart":"yes","caching":"2000"}
+    {"hiddenColumns":[],"token":"$token","browser":"hide","popups":"$pupups","adult":"$adultpages","coinMn":"$coinMn","autoStart":"yes"}
 EOFSS
 
 
